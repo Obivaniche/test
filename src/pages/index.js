882e00b596d85faa5f0bb6705cfd9887dc401f31
Vladislav Smirnov <obivaniche@yandex.ru>
2022-12-08 00:11:41 +0300

    Mobile version added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
+/* Стили */
 import '../pages/index.css';
 
+/* AJAX */
 const descriptonElement = document.querySelector('.info__descripton');
 
 fetch('https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1')
@@ -9,3 +11,43 @@ fetch('https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1'
     .then((res) => {
         descriptonElement.textContent = res;
     });
+
+/* Открытие меню */
+const menuIcon = document.querySelector('.menu__icon');
+const menuContainer = document.querySelector('.menu__container');
+const main = document.querySelector('.main');
+
+if (menuIcon) {
+    menuIcon.addEventListener('click', function (e) {
+        menuIcon.classList.toggle('menu__icon_active');
+        menuContainer.classList.toggle('menu__container_active');
+        main.classList.toggle('main_lock');
+    });
+};
+
+/* Скролл и закрытие меню */
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+            if (menuIcon.classList.contains('menu__icon_active')) {
+                menuIcon.classList.remove('menu__icon_active');
+                menuContainer.classList.remove('menu__container_active');
+                main.classList.remove('main_lock');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            });
+            e.preventDefault();
+        }
+    }
+};
