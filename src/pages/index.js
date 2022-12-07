f8184ced863c38f920f00379f2a4251dee53db1a
Vladislav Smirnov <obivaniche@yandex.ru>
2022-12-07 20:46:12 +0300

    Bugs fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import '../pages/index.css';
 
-descriptonElement = document.querySelector('.info__descripton');
+const descriptonElement = document.querySelector('.info__descripton');
 
 fetch('https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1')
     .then((res) => {
